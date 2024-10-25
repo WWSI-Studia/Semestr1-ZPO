3b78a969aad0bfd232971df3abd29be285d963bf
KStrzechowski <strzechowski.konrad@gmail.com>
2024-10-25 20:16:15 +0200

    Fix types

--- Zadanie2/Zadanie2-SOLID/Zadanie2-SOLID/Master.cs
+++ Zadanie2/Zadanie2-SOLID/Zadanie2-SOLID/Master.cs
@@ -1,12 +1,12 @@
 ﻿namespace Zadanie2_SOLID
 {
-    internal class Master
+    class Master
     {
-        private List<IPerson> ListOfInhabitants { get; set; } = [];
+        private List<IPerson> ListOfPersons { get; set; } = [];
 
         public void Show()
         {
-            foreach (Inhabitant inihabitant in ListOfInhabitants)
+            foreach (IPerson inihabitant in ListOfPersons)
             {
                 Console.WriteLine(inihabitant.Describe());
             }
@@ -17,7 +17,7 @@
             for (int i = 0; i < 10; i++)
             {
                 Random random = new();
-                IPerson inhabitant = this.ListOfInhabitants[random.Next(this.ListOfInhabitants.Count)];
+                IPerson inhabitant = this.ListOfPersons[random.Next(this.ListOfPersons.Count)];
                 Console.WriteLine(inhabitant.Describe());
 
                 while (random.NextDouble() < 0.75)
@@ -29,21 +29,21 @@
 
         public void SetInhabitants()
         {
-            this.ListOfInhabitants.Add(new Inhabitant("Slenderwillow", "f", "Oldoak", "Sunnydaisy", new Gardener()));
-            this.ListOfInhabitants.Add(new Inhabitant("Quickhand", "m", "Bravedeed", "Sunbell", new Knight("sword", "horse")));
-            this.ListOfInhabitants.Add(new Inhabitant("Truefriend", "m", "Highspirit", "Mistymorning", new Knight("spear", "dragon")));
-            this.ListOfInhabitants.Add(new Inhabitant("Greenleaf", "m", "Brownleaf", "Goldendaisy", new Gardener()));
+            this.ListOfPersons.Add(new Inhabitant("Slenderwillow", "f", "Oldoak", "Sunnydaisy", new Gardener()));
+            this.ListOfPersons.Add(new Inhabitant("Quickhand", "m", "Bravedeed", "Sunbell", new Knight("sword", "horse")));
+            this.ListOfPersons.Add(new Inhabitant("Truefriend", "m", "Highspirit", "Mistymorning", new Knight("spear", "dragon")));
+            this.ListOfPersons.Add(new Inhabitant("Greenleaf", "m", "Brownleaf", "Goldendaisy", new Gardener()));
         }
 
-        public void ChangeInhabitantRole(int inhabitantIndex, IRole role)
+        public void ChangeInhabitantRole(int personIndex, IRole role)
         {
-            if (inhabitantIndex < 0 || inhabitantIndex >= this.ListOfInhabitants.Count)
+            if (personIndex < 0 || personIndex >= this.ListOfPersons.Count)
             {
                 Console.WriteLine("Incorrect index.");
             }
             else
             {
-                IPerson inhabitant = this.ListOfInhabitants[inhabitantIndex];
+                IPerson inhabitant = this.ListOfPersons[personIndex];
                 inhabitant.ChangeRole(role);
             }
         }
