9c4e70842914367e7e84abfc5923f4b59a1f4d1e
KStrzechowski <strzechowski.konrad@gmail.com>
2024-12-24 01:50:36 +0100

    Update program.cs

--- Zadanie3-WzorceProjektowe/RestaurantManagment/Program.cs
+++ Zadanie3-WzorceProjektowe/RestaurantManagment/Program.cs
@@ -3,34 +3,93 @@ using RestaurantManagment.Orders;
 using RestaurantManagment.Orders.OrderDecorator;
 using RestaurantManagment.Staff;
 
-Restaurant restaurant = new();
-
-Cook cook1 = new("Andrzej");
-Cook cook2 = new("Beata");
-
-Waiter waiter = new("Jan");
-Waiter waiter2 = new("Karol");
-
-restaurant.AddEmployee(cook1);
-restaurant.AddEmployee(cook2);
-restaurant.AddEmployee(waiter);
-restaurant.AddEmployee(waiter2);
-
-DeliveryAddress address = new("Warszawa");
-
-IOrder[] orders = [
-        new Order("Mielony"),
-        new FreeDeliveryDiscountOrderDecorator(new Order("Schabowy", address)),
-        new CashAmountDiscountOrderDecorator(new Order("Kurczak"), 10),
-        new CashAmountTipOrderDecorator(new PercentageDiscountOrderDecorator(new Order("Sałatka", address), 50), 10),
-    ];
-
-foreach (IOrder order in orders)
+namespace OrderProcessing
 {
-    Console.WriteLine(order);
-    Console.WriteLine();
-    restaurant.AddOrder(order);
+    class Program
+    {
+        private static Cook[] PrepareCooks()
+        {
+            Cook[] employees =
+            [
+                new Cook("Andrzej"),
+                new Cook("Beata"),
+            ];
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee + "\n");
+            }
+
+            return employees;
+        }
+
+        private static Waiter[] PrepareWaiters()
+        {
+            Waiter[] employees =
+            [
+                new Waiter("Jan"),
+                new Waiter("Karol"),
+            ];
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee + "\n");
+            }
+
+            return employees;
+        }
+
+        private static IOrder[] PrepareOrders()
+        {
+            DeliveryAddress address = new("Warszawa");
+
+            IOrder[] orders = [
+                    new Order("Mielony"),
+                    new FreeDeliveryDiscountOrderDecorator(new Order("Schabowy", address)),
+                    new CashAmountDiscountOrderDecorator(new Order("Kurczak"), 10),
+                    new CashAmountTipOrderDecorator(new PercentageDiscountOrderDecorator(new Order("Sałatka", address), 50), 10),
+                ];
+
+            foreach (var order in orders)
+            {
+                Console.WriteLine(order + "\n");
+            }
+
+            return orders;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("-------------CREATING_EMPLOYEES----------------------");
+            Cook[] cooks = PrepareCooks();
+            Waiter[] waiters = PrepareWaiters();
+
+            Console.WriteLine("-------------CREATING_ORDERS----------------------");
+            IOrder[] orders = PrepareOrders();
+
+            Console.WriteLine("-------------CREATING_RESTAURANT----------------------");
+            Restaurant restaurant = new();
+
+            foreach (var cook in cooks)
+            {
+                restaurant.AddEmployee(cook);
+            }
+
+            foreach (var waiter in waiters)
+            {
+                restaurant.AddEmployee(waiter);
+            }
+
+            foreach (var order in orders)
+            {
+                restaurant.AddOrder(order);
+            }
+
+            Console.WriteLine("-------------PROCESSING_ORDERS----------------------");
+            restaurant.StartWorking();
+        }
+    }
 }
-
-restaurant.StartWorking();
-
